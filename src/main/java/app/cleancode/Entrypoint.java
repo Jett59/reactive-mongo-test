17c57c13dcadf06c1b45a2740e17e1165d6e1ca5
Jett Thompson <jett.thompson.59@gmail.com>
2023-02-01 15:53:33 +1100

    fix up the code (then doesn't appear to work properly)

--- src/main/java/app/cleancode/Entrypoint.java
+++ src/main/java/app/cleancode/Entrypoint.java
@@ -13,16 +13,18 @@ import reactor.core.publisher.Mono;
 public class Entrypoint {
     public static void main(String[] args) {
         System.out.println("Creating the client");
-        var mongoClient = MongoClients.create();
-        System.out.println("Getting the database");
-        var database = mongoClient.getDatabase("test");
-        System.out.println("Getting the collection");
-        MongoCollection<Person> peopleCollection = database.getCollection("people", Person.class);
-        System.out.println("Performing queries");
-        Mono.from(peopleCollection.deleteMany(new Document())).then().flatMap(
-                v -> Mono.from(peopleCollection.insertMany(
-                        IntStream.range(0, 100).mapToObj(i -> new Person("Human", Integer.toString(i))).toList())))
-                .then().flatMapMany(v -> Flux.from(peopleCollection.find())).buffer(20).doOnNext(System.out::println)
-                .blockLast();
+        try (var mongoClient = MongoClients.create()) {
+            System.out.println("Getting the database");
+            var database = mongoClient.getDatabase("test");
+            System.out.println("Getting the collection");
+            MongoCollection<Person> peopleCollection = database.getCollection("people", Person.class);
+            System.out.println("Performing queries");
+            var people = Mono.from(peopleCollection.deleteMany(new Document())).flatMap(
+                    v -> Mono.from(peopleCollection.insertMany(
+                            IntStream.range(0, 100).mapToObj(i -> new Person("Human", Integer.toString(i))).toList())))
+                    .flatMapMany(v -> Flux.from(peopleCollection.find())).buffer(20).collectList().block();
+            System.out.println("Done");
+            System.out.println(people);
+        }
     }
 }
